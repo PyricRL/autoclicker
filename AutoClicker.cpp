1a6bca14a376d564aedeedd250e6da23e77a809d
Pyric <97044307+PyricRL@users.noreply.github.com>
2025-01-26 21:24:47 -0600

    Finished basic autoclicker

--- AutoClicker.cpp
+++ AutoClicker.cpp
@@ -9,6 +9,7 @@ enum class clickerType
 };
 
 int enumIndex;
+bool holdingClick = false;
 
 clickerType currentType = clickerType::AUTO;
 
@@ -18,12 +19,19 @@ void autoClicker(int delay)
 {
     while (true)
     {
-        std::cout << "Click" << std::endl;
+        // handle click down and up
+        inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+        SendInput(1, &inputs[0], sizeof(INPUT));
+
+        inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+        SendInput(1, &inputs[0], sizeof(INPUT));
+
+        // wait for the delay
         Sleep(delay);
 
+        // handle exiting the auto clicker
         if (GetAsyncKeyState(VK_F10) & 0x01)
         {
-            std::cout << "broke out" << std::endl;
             break;
         }
     }
@@ -33,8 +41,47 @@ void holdClicker()
 {
     while (true)
     {
-        std::cout << "Click" << std::endl;
+        // allows less sending of packets (less laggy)
+        if (!holdingClick)
+        {
+            // hold down click
+            inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+            SendInput(1, &inputs[0], sizeof(INPUT));
+            holdingClick = true;
+        }
 
+        // handle exiting the hold clicker
+        if (GetAsyncKeyState(VK_F10) & 0x01)
+        {
+            // stop holding click
+            inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+            SendInput(1, &inputs[0], sizeof(INPUT));
+            holdingClick = false;
+
+            break;
+        }
+    }
+}
+
+void holdBreakClicker(int delay)
+{
+    while (true)
+    {
+        // Simulate mouse button down
+        inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+        SendInput(1, &inputs[0], sizeof(INPUT));
+
+        // Hold for 2.5 seconds
+        Sleep(delay);
+
+        // Simulate mouse button up
+        inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+        SendInput(1, &inputs[0], sizeof(INPUT));
+
+        // Small delay before the next cycle
+        Sleep(50);
+
+        // handle exiting the hold break clicker
         if (GetAsyncKeyState(VK_F10) & 0x01)
         {
             std::cout << "broke out" << std::endl;
@@ -60,7 +107,6 @@ void cycleStates()
         }
 
         currentType = static_cast<clickerType>(enumIndex); // Update the currentType
-        std::cout << enumIndex << " type" << std::endl;
     }
 }
 
@@ -77,7 +123,20 @@ int main()
     {
         if (GetAsyncKeyState(VK_F9) & 0x01)
         {
-            autoClicker(1000);
+            if (currentType == clickerType::AUTO)
+            {
+                autoClicker(1);
+            }
+
+            if (currentType == clickerType::HOLD)
+            {
+                holdClicker();
+            }
+
+            if (currentType == clickerType::HOLDBREAK)
+            {
+                holdBreakClicker(2500);
+            }
         }
 
         cycleStates();
